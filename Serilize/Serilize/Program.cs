83b0d50a8459d1e90781ad0bb85cda9a807e0bfd
sumton.zhao <sumton.zhao@dazong.com>
2018-06-15 17:18:32 +0800

    对比

---
+++ Serilize/Serilize/Program.cs
@@ -0,0 +1,18 @@
+﻿using BenchmarkDotNet.Running;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Serilize
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var summary = BenchmarkRunner.Run<Test>();
+            Console.Read();
+        }
+    }
+}
